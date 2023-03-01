21a1b9b9d9e7284872a968bfc39ce8bed6c7bd2b
jake497726 <jung.jin@northeastern.edu>
2023-03-01 11:14:42 -0500

    Update main.js

--- main.js
+++ main.js
@@ -55,7 +55,7 @@ d3.csv('data/iris.csv').then((data)=> {
 
 
     // Add the data points
-    FRAME1.selectAll("circle")
+    let scatter = FRAME1.selectAll("circle")
        .data(data)
        .enter()
        .append("circle")
@@ -80,28 +80,22 @@ d3.csv('data/iris.csv').then((data)=> {
         .attr("text-anchor", "middle")
         .text("Petal_Length vs Sepal_Length");
 
-  });
 
-
-
-
-// middle column
-const FRAME2 = d3.select('#vis2')
+    // middle column
+    const FRAME2 = d3.select('#vis2')
                   .append('svg')
                   .attr('height', FRAME_HEIGHT)
                   .attr('width', FRAME_WIDTH)
                   .attr('class', 'frame')
 
 
-// Reading from file
-d3.csv('data/iris.csv').then((data)=> {
 
-    const y = d3.scaleLinear()
+    const y1 = d3.scaleLinear()
                             .domain([0, d3.max(data, d => +d.Petal_Width)+0.2])
                             // .domain(data.map((d) => { return d.Petal_Length; }))
                             .range([VIS_HEIGHT,0]);
 
-    const x = d3.scaleLinear()
+    const x1 = d3.scaleLinear()
                             .domain([0, d3.max(data, d => +d.Sepal_Width)+0.5])
                             // .domain(data.map((d) => { return d.Sepal_Length; }))
                             .range([0,VIS_WIDTH]);
@@ -117,7 +111,7 @@ d3.csv('data/iris.csv').then((data)=> {
      //move the axis down to the page
         .attr("transform", "translate(" + MARGINS.left + 
               "," + (VIS_HEIGHT + MARGINS.top) + ")") 
-        .call(d3.axisBottom(x))
+        .call(d3.axisBottom(x1))
         .attr("font-size", '20px'); 
 
 
@@ -128,63 +122,108 @@ d3.csv('data/iris.csv').then((data)=> {
     //move the axis down to the page
         .attr("transform", "translate(" + (MARGINS.left) + 
               "," + (MARGINS.top) + ")") 
-        .call(d3.axisLeft(y)) 
+        .call(d3.axisLeft(y1)) 
           .attr("font-size", '20px'); 
-
-
-    // Add the data points
-    FRAME2.selectAll("circle")
-       .data(data)
-       .enter()
-       .append("circle")
-        .attr("cx", (d) => {
-            return (MARGINS.left + x(d.Sepal_Width))})
-        .attr("cy", (d) => {
-            return (MARGINS.top + y(d.Petal_Width))})
-        .attr("r", 5)
-        .attr("opacity", 0.5)
-        .attr("fill", d => color(d.Species));
-
-
-
-   
-    // Add the chart title
+    
+        // Add the chart title
     FRAME2.append("text")
         .attr("x", 300)
         .attr("y", 30)
         .attr("text-anchor", "middle")
         .text("Petal_Width vs Sepal_Width");
 
-    var svg = d3.select("#vis2")
-    .append("svg")
-    .attr("width", width + margin.left + margin.right)
-      .attr("height", height + margin.top + margin.bottom)
-     .append("g")
-      .attr("transform",
-            "translate(" + margin.left + "," + margin.top + ")");
+
+    // Add the data points
+    let scatter1 = FRAME2.selectAll("circle")
+       .data(data)
+       .enter()
+       .append("circle")
+        .attr("cx", (d) => {
+            return (MARGINS.left + x1(d.Sepal_Width))})
+        .attr("cy", (d) => {
+            return (MARGINS.top + y1(d.Petal_Width))})
+        .attr("r", 5)
+        .attr("opacity", 0.5)
+        .attr("fill", d => color(d.Species));
 
 
-  // Add brushing
-    svg.call(d3.brush()
-        .extent( [ [0,0], [FRAME_WIDTH,FRAME_HEIGHT] ] ) // initialise the brush area: start at 0,0 and finishes at width,height: it means I select the whole graph area
-        .on("start brush", updateChart)) // Each time the brush selection changes, trigger the 'updateChart' function
 
-      // Function that is triggered when brushing is performed
-      function updateChart() {
-       extent = d3.event.selection
-       circle.classed("selected", function(d){ return isBrushed(extent, x(d.Sepal_Length), y(d.Petal_Length) ) } )
-      }
+    // Add brushing
+    FRAME2.call(d3.brush()
+             .extent( [ [0,0], [FRAME_WIDTH,FRAME_HEIGHT] ] )
+             .on("start brush", updateChart)
+           );
 
-      // A function that return TRUE or FALSE according if a dot is in the selection or not
-     function isBrushed(brush_coords, cx, cy) {
-          var x0 = brush_coords[0][0],
-               x1 = brush_coords[1][0],
-               y0 = brush_coords[0][1],
-            y1 = brush_coords[1][1];
-          return x0 <= cx && cx <= x1 && y0 <= cy && cy <= y1;
-     }
+    // Function that is triggered when brushing is performed
+    function updateChart() {
+        extent = d3.event.selection;
+        scatter.classed("selected", function(d){ return isBrushed(extent, (x1(d.Sepal_Width)+ MARGINS.left), (y1(d.Petal_Width)+MARGINS.top) } );
+        scatter1.classed("selected", function(d){ return isBrushed(extent, (x1(d.Sepal_Width)+ MARGINS.left), (y1(d.Petal_Width)+MARGINS.top) } );
+        bars.classed("selected", function(d){ return isBrushed(extent, (x1(d.Sepal_Width)+ MARGINS.left), (y1(d.Petal_Width)+MARGINS.top) } );
 
-  });
+    // A function that returns TRUE or FALSE according if a dot is in the selection or not
+    function isBrushed(brush_coords, cx, cy) {
+        var x0 = brush_coords[0][0],
+            x1 = brush_coords[1][0],
+            y0 = brush_coords[0][1],
+            y1 = brush_coords[1][1]; 
+        return x0 <= cx && cx <= x1 && y0 <= cy && cy <= y1;
+    }
 
 
+    const FRAME3 = d3.select("#vis3") 
+                  .append("svg") 
+                    .attr("height", FRAME_HEIGHT)   
+                    .attr("width", FRAME_WIDTH)
+                    .attr("class", "frame");
+
+
+    const ySCALE = d3.scaleLinear() 
+                       .domain([0, 60])  
+                       .range([VIS_HEIGHT, 0]);
+
+    const xSCALE = d3.scaleBand()
+                        .range([ 0, VIS_WIDTH ])
+                        .domain(data.map(function(d) { return d.Species; }))
+                        .padding(0.45);
+
+    const BAR_WIDTH = 60;
+
+    var color = d3.scaleOrdinal()
+                  .domain(["setosa", "versicolor", "virginica"])
+                  .range(["#1f77b4", "#ff7f0e", "#2ca02c"]);
+
+    FRAME3.append("g")
+         .attr("transform", "translate(" + MARGINS.left + 
+          "," + (VIS_HEIGHT+ MARGINS.bottom) + ")")
+         .call(d3.axisBottom(xSCALE))
+         .selectAll("text")
+           .attr("font-size", "20px");
+
+    FRAME3.append("g")
+       .attr("transform", "translate(" + MARGINS.left + "," + MARGINS.top + ")") 
+         .call(d3.axisLeft(ySCALE))
+         .selectAll("text")
+           .attr("font-size", "20px");
+
+
+    let bars = FRAME3.append("g").selectAll("bar")
+      .data(data)
+      .enter()
+      .append("rect")
+        .attr("x", function(d) { return xSCALE(d.Species) + MARGINS.left; })
+        .attr("y", function(d) { return ySCALE(50) + MARGINS.top; })
+        .attr("width", BAR_WIDTH)
+        .attr("height", function(d) { return VIS_HEIGHT - ySCALE(50); })
+        .attr("fill", (d) => color(d.Species))
+                .attr("opacity", 1);
+
+
+    // Add the chart title
+    FRAME3.append("text")
+        .attr("x", 300)
+        .attr("y", 30)
+        .attr("text-anchor", "middle")
+        .text("setosa vs versicolor vs virginica");
+});
 
