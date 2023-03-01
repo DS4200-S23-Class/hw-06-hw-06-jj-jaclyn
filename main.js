28666c08904dc711eb8f0a9edd50964dfad73d90
jake497726 <jung.jin@northeastern.edu>
2023-02-28 21:51:55 -0500

    Update main.js

--- main.js
+++ main.js
@@ -162,3 +162,82 @@ d3.csv('data/iris.csv').then((data)=> {
 
   });
 
+
+// right column
+const FRAME3 = d3.select('#vis3')
+                  .append('svg')
+                  .attr('height', FRAME_HEIGHT)
+                  .attr('width', FRAME_WIDTH)
+                  .attr('class', 'frame')
+
+
+// Reading from file
+d3.csv('data/iris.csv').then((data)=> {
+
+    const y = d3.scaleLinear()
+                            .domain([0, d3.max(data, d => +d.Petal_Width)+0.2])
+                            // .domain(data.map((d) => { return d.Petal_Length; }))
+                            .range([VIS_HEIGHT,0]);
+
+    const x = d3.scaleLinear()
+                            .domain([0, d3.max(data, d => +d.Sepal_Width)+0.5])
+                            // .domain(data.map((d) => { return d.Sepal_Length; }))
+                            .range([0,VIS_WIDTH]);
+
+    // Create a color scale
+    var color = d3.scaleOrdinal()
+                  .domain(["setosa", "versicolor", "virginica"])
+                  .range(["#1f77b4", "#ff7f0e", "#2ca02c"]);
+
+
+    // add x axis 
+   FRAME2.append("g") 
+     //move the axis down to the page
+        .attr("transform", "translate(" + MARGINS.left + 
+              "," + (VIS_HEIGHT + MARGINS.top) + ")") 
+        .call(d3.axisBottom(x))
+        .attr("font-size", '20px'); 
+
+
+
+
+  // add y axis
+  FRAME2.append("g") 
+    //move the axis down to the page
+        .attr("transform", "translate(" + (MARGINS.left) + 
+              "," + (MARGINS.top) + ")") 
+        .call(d3.axisLeft(y)) 
+          .attr("font-size", '20px'); 
+
+
+    // Add the data points
+    FRAME2.selectAll("circle")
+       .data(data)
+       .enter()
+       .append("circle")
+        .attr("cx", (d) => {
+            return (MARGINS.left + x(d.Sepal_Width))})
+        .attr("cy", (d) => {
+            return (MARGINS.top + y(d.Petal_Width))})
+        .attr("r", 5)
+        .attr("opacity", 0.5)
+        .attr("fill", d => color(d.Species));
+
+           // .attr("cx", function(d) { return petal_length(d.Sepal_Length) + MARGINS.bottom; })
+           // .attr("cy", function(d) { return sepal_length(d.Petal_Length) + MARGINS.left; })
+           // .attr("r", 5)
+           // .style("fill", function(d) { return color(d.Species); })
+           // .style("opacity", 0.5);
+
+
+   
+    // Add the chart title
+    FRAME2.append("text")
+        .attr("x", 300)
+        .attr("y", 30)
+        .attr("text-anchor", "middle")
+        .text("Petal_Width vs Sepal_Width");
+
+  });
+
+
